8dbb3196a75b99f9b6da426ecf1f3700fd07c43e
Lucie <lye10@hawk.iit.edu>
2024-02-23 15:07:44 -0600

    Convert to SQLite

---
+++ itmd542-express-sqlite/routes/contacts.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var { body, validationResult } = require("express-validator");
+var router = express.Router();
+
+const repo = require("../src/repository");
+
+/* GET list of contacts. */
+router.get("/", function (req, res, next) {
+  const data = repo.findAll();
+
+  res.render("contacts", { title: "List of the contacts", contacts: data });
+});
+
+/* GET form to add a contact. */
+router.get("/add", function (req, res, next) {
+  res.render("contacts_input", { title: "Add a new contact" });
+});
+
+/* POST form to add a contact. */
+router.post("/add",
+  body("firstname").trim(),
+  body("lastname").trim(),
+  body("email").trim(),
+  body("notes").trim(),
+  function (req, res, next) {
+    repo.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      notes: req.body.notes,
+    });
+    res.redirect("/contacts");
+  }
+);
+
+/* GET a single contact. */
+router.get("/:uuid", function (req, res, next) {
+  const contact = repo.findById(req.params.uuid);
+
+  if (contact) {
+    res.render("contact", {
+      title: `${contact.firstname} ${contact.lastname}`,
+      contact: contact,
+    });
+  }
+});
+
+/* GET contact delete */
+router.get("/:uuid/delete", function (req, res, next) {
+  repo.deleteById(req.params.uuid);
+  res.redirect("/contacts");
+});
+
+/* GET contact edit */
+router.get("/:uuid/edit", function (req, res, next) {
+  const contact = repo.findById(req.params.uuid);
+
+  res.render("contacts_input", {
+    title: `Edit ${contact.firstname} ${contact.lastname}`,
+    contact: contact,
+  });
+});
+
+/* POST contact edit */
+router.post("/:uuid/edit",
+  body("firstname").trim(),
+  body("lastname").trim(),
+  body("email").trim(),
+  body("notes").trim(),
+  function (req, res, next) {
+  const updatedContact = {
+    id: req.params.uuid,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    notes: req.body.notes,
+    date: (new Date()).toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true })
+  };
+
+  repo.update(updatedContact);
+  res.redirect("/contacts");
+});
+
+module.exports = router;
---
+++ itmd542-express-sqlite/src/repository.js
@@ -0,0 +1,41 @@
+const path = require("node:path");
+const betterSqlite3 = require("better-sqlite3");
+
+const db = new betterSqlite3(path.join(__dirname, "../data/contacts.sqlite"));
+const createDb = db.prepare(
+  "CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, email TEXT, notes TEXT, date TEXT DEFAULT (strftime('%Y-%m-%d %I:%M:%S %p', 'now', 'localtime')))"
+);
+createDb.run();
+
+const repo = {
+  findAll: () => {
+    return db.prepare("SELECT * FROM contacts").all();
+  },
+  findById: (uuid) => {
+    return db.prepare("SELECT * FROM contacts WHERE id = ?").get(uuid);
+  },
+
+  create: (contact) => {
+    db.prepare(
+      "INSERT INTO contacts (firstname, lastname, email, notes) VALUES (?, ?, ?, ?)"
+    ).run(contact.firstname, contact.lastname, contact.email, contact.notes);
+  },
+
+  deleteById: (uuid) => {
+    db.prepare("DELETE FROM contacts WHERE id = ?").run(uuid);
+  },
+
+  update: (updatedContact) => {
+    db.prepare(
+      "UPDATE contacts SET firstname = ?, lastname = ?, email = ?, notes = ?, date = strftime('%Y-%m-%d %I:%M:%S %p', 'now', 'localtime') WHERE id = ?"
+    ).run(
+      updatedContact.firstname,
+      updatedContact.lastname,
+      updatedContact.email,
+      updatedContact.notes,
+      updatedContact.id
+    );
+  },
+};
+
+module.exports = repo;
